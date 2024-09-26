9240324f89970674198d2f8ef9da0ab430ecf15e
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 21:19:30 -0700

    waterfalls

---
+++ waterfalls_list.py
@@ -0,0 +1,8 @@
+waterfalls = ["Niagara Falls", "Victoria Falls", "Iguazu Falls", "Angel Falls", "Yosemite Falls", "Plitvice Falls", "Sutherland Falls", "Huangguoshu Waterfall", "Multnomah Falls", "Jog Falls"]
+print(waterfalls)
+print("the 8th index is",waterfalls[7])
+waterfalls[4] = "niagara falls"
+print(waterfalls)
+del waterfalls [8]
+print(waterfalls)
+print("the last index is ",waterfalls[-1])
