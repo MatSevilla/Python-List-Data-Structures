bfea9b81e2c566187df1551e369801e075eae6e8
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:31:22 -0700

    Best_species

---
+++ animal_species.py
@@ -0,0 +1,7 @@
+animalspecies = ["African Elephant", "Bald Eagle", "Great White Shark", "Bengal Tiger", "Giant Panda", "Emperor Penguin", "Blue Whale"]
+print(animalspecies)
+print("the 2nd list is",animalspecies[1])
+animalspecies[4] = "gorilla"
+print(animalspecies)
+del animalspecies[5]
+print("the last index is ",animalspecies[-1])
