220d80c877826399c509eafa3d6fb6681d0aa779
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:24:28 -0700

    unique_bird

---
+++ bird_list.py
@@ -0,0 +1,8 @@
+bird = ["Bald Eagle", "Philippine Eagle", "Peregrine Falcon", "Flamingo", "Peacock", "Ostrich", "Penguin", "Swan", "Parrot", "Kingfisher", "Owl", "Albatross"]
+print(bird)
+print("the 9th index is",bird[8])
+bird[5] = "eagle"
+del bird [7]
+print(bird)
+print(bird[2:7])
+print("the last index is",bird[-1])
