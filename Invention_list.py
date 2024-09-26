6001556f59896f576ab71a70d7a23dc5d3da2a40
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:44:04 -0700

    Greatest_Invention

---
+++ Invention_list.py
@@ -0,0 +1,8 @@
+inventions = ["Telephone", "Light Bulb", "Printing Press", "Airplane", "Internet", "Steam Engine", "Penicillin", "Electric Motor"]
+print(inventions)
+print("the 6th index is",inventions[5])
+inventions[1] = "light bulb"
+print(inventions)
+del inventions[4]
+print(inventions)
+print("the last index is ",inventions[-1])
