04344958805717e84d7ccca4d1682fdc7cef1e40
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:23:48 -0700

    Greatest_mountain

---
+++ mountain_list.py
@@ -0,0 +1,8 @@
+mountain = ["everest", "annapurna", "denali", "makalu", "k2", "mount black burn", "mount elbrus"]
+print(mountain)
+print("the 5th index is",mountain[4])
+mountain[2] = "everest"
+print(mountain)
+del mountain[5]
+print(mountain)
+print("the last index is",mountain[-1])
