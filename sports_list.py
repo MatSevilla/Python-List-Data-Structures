88f458140887ebfc466c1400a0e36987a971bf0b
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:58:24 -0700

    sports_list

---
+++ sports_list.py
@@ -0,0 +1,8 @@
+sport = ["Tennis", "Basketball", "Soccer", "Swimming", "Rugby", "Cricket", "Baseball", "Golf", "Ice Hockey", "water polo"]
+print(sport)
+print("th 6th index is",sport[5])
+sport[3] = "basketball"
+print(sport)
+del sport[8]
+print(sport)
+print("the last index is",sport[-1])
