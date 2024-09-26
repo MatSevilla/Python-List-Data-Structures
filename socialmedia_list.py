97f04b0af86f761c46fe26fbbe1e525dcd004480
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:56:32 -0700

    Social_media_platforms

---
+++ socialmedia_list.py
@@ -0,0 +1,8 @@
+platforms = ["Facebook", "Twitter", "X", "LinkedIn", "TikTok", "Snapchat", "YouTube", "Pinterest", "Reddit", "Instagram"]
+print(platforms)
+print("the 5th index is",platforms[4])
+platforms[2] = "instagram"
+print(platforms)
+del platforms[7]
+print(platforms)
+print("the last index is",platforms[-1])
