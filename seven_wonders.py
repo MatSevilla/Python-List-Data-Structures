3186a5ec3f6b332eeb902ec207e12e953f0174ea
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 21:23:53 -0700

    seven_wonders

---
+++ seven_wonders.py
@@ -0,0 +1,8 @@
+seven_wonders = ["Great Wall of China", "Petra", "Christ the Redeemer", "Machu Picchu", "Chichen Itza", "Roman Colosseum", "Taj Mahal"]
+print(seven_wonders)
+print("the 4th index is",seven_wonders[3])
+seven_wonders[1] = "GREAT WALL OF CHINA"
+print(seven_wonders)
+del seven_wonders[4]
+print(seven_wonders)
+print("the last index is",seven_wonders[-1])
