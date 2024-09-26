cfecb0eae4ba07265ab2a467ba0675d25fc244b4
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:53:16 -0700

    celebration_worldwide

---
+++ celebration_list.py
@@ -0,0 +1,8 @@
+celebrations = ["Diwali", "Christmas", "Eid al-Fitr", "Holi", "Thanksgiving", "Mardi Gras", "Oktoberfest", "Chinese New Year", "Hanukkah", "Day of the Dead"]
+print(celebrations)
+print("the 5th index is",celebrations[4])
+celebrations[2] = "thansk giving"
+print(celebrations)
+del celebrations [6]
+print(celebrations)
+print("the last index is",celebrations[-1])
