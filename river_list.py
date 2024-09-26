864b9f90feae38cfe58c24e717ca37ad27bb1b43
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 21:17:23 -0700

    river

---
+++ river_list.py
@@ -0,0 +1,8 @@
+rivers = ["Amazon River", "Nile River", "Yangtze River", "Mississippi River", "Danube River", "Ganges River", "Thames River", "Rio Grande", "Volga River", "Seine River"]
+print(rivers)
+print("the 6th index is",rivers [5])
+rivers[7] = "Nile River"
+print(rivers)
+del rivers [8]
+print(rivers)
+print("the last index is",rivers[-1])
