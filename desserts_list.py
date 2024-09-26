b11a9fd2efbcbfc8c5a174d104a25790a1da51d0
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:46:17 -0700

    sweets

---
+++ desserts_list.py
@@ -0,0 +1,8 @@
+desserts = ["Chocolate Cake", "Cheesecake", "Tiramisu", "Apple Pie", "Pavlova", "Brownies", "Crème Brûlée", "Gelato", "Macarons", "Baklava"]
+print(desserts)
+print("the 4th index is",desserts[3])
+desserts[2] = "ice cream"
+print(desserts)
+del desserts[6]
+print(desserts)
+print("the last index is",desserts[-1])
