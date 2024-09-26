dfcef237ca7fe04a3ce6f3396f398c7fa357a0a5
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:18:40 -0700

    JOb_titles

---
+++ jobtitle_list.py
@@ -0,0 +1,8 @@
+job_title = ["Software Engineer", "Project Manager", "Data Analyst", "Graphic Designer", "Nurse", "Teacher", "Marketing Specialist", "Sales Representative", "Accountant", "Human Resources Manager"]
+print(job_title)
+print("the 6th index is",job_title[5])
+job_title[4] = "software engineer"
+print(job_title)
+del job_title[8]
+print(job_title)
+print("the last index is",job_title[-1])
