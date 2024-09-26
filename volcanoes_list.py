bd9573dd46be8f924900ed313b7aae8f5b9327cb
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 21:15:48 -0700

    volcano

---
+++ volcanoes_list.py
@@ -0,0 +1,8 @@
+volcanoes = ["Mount Vesuvius", "Kilauea", "Mount St. Helens", "Mount Fuji", "Mauna Loa", "Eyjafjallajökull", "Mount Etna"]
+print(volcanoes)
+print("the 4th index is",volcanoes[3])
+volcanoes[2] = "Mount Vesuvius"
+print(volcanoes)
+del volcanoes[4]
+print(volcanoes)
+print("the last index is",volcanoes[-1])
