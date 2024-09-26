35109376b5c7c3c45d15def8697d90e360e2ab19
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:13:22 -0700

    modern_technology

---
+++ modern_tech_list..py
@@ -0,0 +1,9 @@
+modern_technology = ["Smartphone", "Tablet", "Electric Car", "3D Printer", "Wearable Fitness Tracker", "Artificial Intelligence", "Virtual Reality Headset", "Blockchain Technology", "Drones", "Smart Home Assistants", "Streaming Services", "Biometric Authentication", "Augmented Reality Glasses", "Robotics", "Quantum Computing"]
+print(modern_technology)
+print("the 11th index is ",modern_technology[10])
+modern_technology[7] = 'smart phone'
+print(modern_technology)
+del modern_technology[-2]
+print(modern_technology)
+print(modern_technology[4:10])
+print("the last index is",modern_technology[-1])
