faa00b1c5ef81b12bdf55c93a301c9709339cee4
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:08:42 -0700

    School_subject

---
+++ school_subject_list.py
@@ -0,0 +1,8 @@
+subjects = ["Filipino", "English", "Mathematics", "Science", "Social Studies", "Araling Panlipunan", "MAPEH", "Technology and Livelihood Education", "Values Education", "Computer Education", "Business Studies", "Environmental Science"]
+print(subjects)
+print("the 6th index is",subjects[5])
+subjects[7] = "mathematics"
+print(subjects)
+del subjects[3]
+print(subjects[1:5])
+print("the last index is",subjects[-1])
