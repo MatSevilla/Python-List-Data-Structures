babbf06f47fdffa01fef078a26e3b1f9a5d80cd7
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:15:33 -0700

    programming_language

---
+++ language.py
@@ -0,0 +1,9 @@
+language = ["c++", "c#", "java", "python", "java script", "swift", "php", "sql", "css", "r", "ruby", "scala", "mathlab"]
+print(language)
+print("the 11th index is ",language[10])
+language[8] = "python"
+print(language)
+del language[12]
+print(language)
+print(language[4:12])
+print("the last index is",language[-1])
