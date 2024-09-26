3c35befd4549cec963925877580d9a37d94ae90d
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:58:48 -0700

    landmarks

---
+++ landmarks_list.py
@@ -0,0 +1,8 @@
+landmarks = ["Grand Canyon", "Great Barrier Reef", "Mount Everest", "Niagara Falls", "Machu Picchu", "Sahara Desert", "Yosemite Valley", "Victoria Falls", "Angkor Wat", "Lake Baikal"]
+print(landmarks)
+print("the 8th index is",landmarks[7])
+landmarks[4] = "grand canyon"
+print(landmarks)
+del landmarks[8]
+print(landmarks)
+print("the last index is",landmarks[-1])
