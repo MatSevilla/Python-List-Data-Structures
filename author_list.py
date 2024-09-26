250738ed08b6651f9151ebcb01999d8d79a6fad7
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:17:57 -0700

    Manga_authors

---
+++ author_list.py
@@ -0,0 +1,9 @@
+author = ["eiichiro oda", "tite kubo", "masashi kishimoto", "hirohiko aiki", "naoki urasawa", "tatsuki fujimoto", "takeshi obata", "tako saito", "hayao miyazaki", "q hayashida", "io sakisaka", "natsuki tayaka"]
+print(author)
+print("the 6th index is",author[5])
+author[3] = "mark twain"
+print(author)
+del author[9]
+print(author)
+print(author[3:8])
+print("the last index is",author[-1])
