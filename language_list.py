bd06f517f8d50388a2bdc55579a1d82b3ea76bf6
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:20:04 -0700

    Spoken_language

---
+++ language_list.py
@@ -0,0 +1,9 @@
+language = ["english", "spanish", "chinise", "arabic", "russian", "french", "persian", "german", "malay", "portugueuse", "italian", "turkish", "lahnda", "tamil", "urdu", "korean", "hindi", "bengali", "japanese", "vietnamese" ]
+print(language)
+print("the 13th index is",language[12])
+language[9] = "spanish"
+print(language)
+del language[-4]
+print(language)
+print(language[5:11])
+print("the last index is",language[-1])
