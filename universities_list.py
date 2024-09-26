924f9dfbdb2aaeb1f44d45966a05337b99e4ca69
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:51:16 -0700

    universities

---
+++ universities_list.py
@@ -0,0 +1,8 @@
+universities = ["Harvard University", "Stanford University", "Massachusetts Institute of Technology", "California Institute of Technology", "University of Oxford", "University of Cambridge", "University of Chicago", "Columbia University", "Yale University", "Princeton University"]
+print(universities)
+print("the 6th index is",universities[5])
+universities[4] = "Harvard university"
+print(universities)
+del universities [-2]
+print(universities)
+print("the last index is",universities[-2])
