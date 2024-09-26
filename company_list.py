4901247d6539d67739b4c9263cd14521205150b5
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:26:13 -0700

    Top_companys

---
+++ company_list.py
@@ -0,0 +1,7 @@
+company = ["tesla", "facebook", "x", "youtube", "microsoft", "samsung", "sony", "walmart", "chanel", "nintendo"]
+print(company)
+print("the 7th index is",company[6])
+company[4] = "apple"
+del company[-1]
+print(company)
+print("the last index is",company[-1])
