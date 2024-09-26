3eec8aead260863d4b8008f363442676027caf29
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:10:46 -0700

    tools

---
+++ carpenter_tools_list.py
@@ -0,0 +1,8 @@
+tools = ["Hammer", "Saw", "Chisel", "Tape Measure", "Square", "Level", "Drill", "Screwdriver", "Clamps", "Router"]
+print(tools)
+print("the 4th index is",tools[3])
+tools[6] = "hammer"
+print(tools)
+del tools [4]
+print(tools)
+print("the last index is",tools[-1])
