9f0d87507c64f86e92b5f8e18755f4f210233c46
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 21:21:52 -0700

    bridges

---
+++ bridges_list.py
@@ -0,0 +1,8 @@
+bridges = ["Golden Gate Bridge", "Brooklyn Bridge", "Tower Bridge", "Sydney Harbour Bridge", "Forth Bridge", "Charles Bridge", "Acropolis Bridge", "Gateshead Millennium Bridge"]
+print(bridges)
+print("the 3rd index is",bridges[2])
+bridges[5] = "Golden Gate Bridge"
+print(bridges)
+del bridges[6]
+print(bridges)
+print('the last index is',bridges[-1])
