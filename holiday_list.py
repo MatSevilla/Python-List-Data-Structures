b1776a9b0f40ee990e7c70b145bcdbf2ef39a3ba
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:46:29 -0700

    Holidays_list

---
+++ holiday_list.py
@@ -0,0 +1,7 @@
+holiday = ["New Year's Day", "Christmas", "Thanksgiving", "Independence Day", "Easter", "Halloween", "Labor Day", "Valentine's Day", "Hanukkah", "Diwali", "Lunar New Year", "St. Patrick's Day"]
+print(holiday)
+print("the 9th index is",[8])
+holiday[2] = "christmas"
+print(holiday)
+print(holiday[1:7])
+print("the last index is",holiday[-1])
