f382cdaf02519d1c159861a33caf34a1cc73f9d8
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:05:51 -0700

    International_actors

---
+++ actors_list.py
@@ -0,0 +1,8 @@
+actors = ["Leonardo DiCaprio", "Meryl Streep", "Denzel Washington", "Scarlett Johansson", "Brad Pitt", "Emma Stone", "Matthew McConaughey", "Hugh Jackman", "Natalie Portman", "Will Smith"]
+print(actors)
+print("the 7th index is",actors[6])
+actors[2] = "leonardo dicarpio"
+print(actors)
+del actors[7]
+print(actors)
+print("the last index is",actors[-1])
