55cf5fc763a6a2d047fd7e868d6a6194b3db5904
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:36:28 -0700

    Historical_figures

---
+++ historical_figures.py
@@ -0,0 +1,8 @@
+figures = ["Julius Caesar", "Cleopatra", "George Washington", "Mahatma Gandhi", "Napoleon Bonaparte", "Albert Einstein", "Leonardo da Vinci", "Martin Luther King Jr.", "Marie Curie", "Nelson Mandela"]
+print(figures)
+print("the 8th index is ", figures[7])
+figures[3] = "Nelson Mandela"
+print(figures)
+del figures[6]
+print(figures)
+print("the last index is",figures[-1])
