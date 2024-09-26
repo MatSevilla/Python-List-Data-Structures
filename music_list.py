11f46e8a10f54455e8dba78da2fdb5bf952e2b02
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:00:37 -0700

    music_genre

---
+++ music_list.py
@@ -0,0 +1,8 @@
+music = ["Jazz", "Rock", "Hip Hop", "Classical", "Pop", "Reggae", "Blues", "Country", "Electronic", "R&B", "Folk", "Metal", "Latin", "Punk", "Gospel"]
+print(music)
+print("the 10th index is",music[9])
+music[4] = "jazz"
+print(music)
+del music[6]
+print(music[2:8])
+print("the last index is",music[-1])
