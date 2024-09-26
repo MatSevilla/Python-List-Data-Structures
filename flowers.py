dfe91c2f8827a24145cf52beb85cf5f00a3d6ba3
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:34:09 -0700

    Flowers_list

---
+++ flowers.py
@@ -0,0 +1,8 @@
+flower = ["lily", "rose", "sunflower", "iris", "marigold", "daisy", "jasmin", "camellia", "lotus", "poppy"]
+print(flower)
+print("the 5th index is",flower[4])
+flower[7] = "rose"
+print(flower)
+del flower[1]
+print(flower)
+print("the last index is",flower[-1])
