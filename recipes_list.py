79cdb229633e4a98b2c7f68a5f578af9ce301609
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:30:50 -0700

    recipes

---
+++ recipes_list.py
@@ -0,0 +1,7 @@
+recipes = ["Spaghetti Carbonara", "Chicken Tikka Masala", "Beef Stroganoff", "Vegetable Stir-Fry", "Tacos al Pastor", "Mushroom Risotto", "Pancakes", "Caesar Salad", "Chili Con Carne", "Lemon Tart", "Egg Fried Rice", "Grilled Salmon", "Stuffed Bell Peppers", "Lasagna", "Chocolate Chip Cookies"]
+print(recipes)
+print("the 12th index is",recipes[-3])
+del recipes [-10]
+print(recipes)
+print(recipes[4:10])
+print("the last index is ",recipes[-1])
