bc44633f051725b5214d5fa170f38ffafa431a73
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:48:44 -0700

    Festival_list

---
+++ festival_list.py
@@ -0,0 +1,7 @@
+festival = ["Diwali", "Carnival", "Oktoberfest", "Holi", "Mardi Gras", "Chinese New Year", "La Tomatina", "Gion Matsuri", "Running of the Bulls", "Songkran", "Day of the Dead", "Burning Man", "Edinburgh Festival Fringe", "Bastille Day", "Glastonbury Festival"]
+print(festival)
+print("the 7th index is ",festival[6])
+festival[10] = "diwali"
+print(festival)
+print(festival[4:12])
+print("the last index is",festival[-1])
