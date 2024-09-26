68902fbba86b218084235b66b1088657f90737bf
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 21:02:04 -0700

    animalspeed_list

---
+++ animal_speed.py
@@ -0,0 +1,8 @@
+animals_speeds = ["Horse - 120 km/h", "Lion - 50 km/h", "Tiger - 30 km/h", "Gazelle - 60 km/h", "Horse - 55 km/h", "Greyhound - 45 km/h", "Elephant - 25 km/h", "Kangaroo - 40 km/h", "Wildebeest - 30 km/h", "American Bison - 35 km/h", "Rhinoceros - 30 km/h", "Zebra - 40 km/h"]
+print(animals_speeds)
+print("the 7th index is",animals_speeds[6])
+animals_speeds[8] = "Cheetah - 120 km/h"
+print(animals_speeds)
+del animals_speeds [9]
+print(animals_speeds)
+print("the last index is",animals_speeds[-1])
