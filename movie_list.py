68063814cf1d41a5360bdc18c94ac27d77a5f560
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:04:46 -0700

    Top_movie_list

---
+++ movie_list.py
@@ -0,0 +1,9 @@
+movie = ["pacific rim", "hellow wold", "silent voice", "Hacksaw ridge","spider man", "restart the earth", "record fo ragnarok", "spartan","home alone", "inside out", "alive ", "Zombie apocalypse", "train to busan", "dawn of the dead", "1917", "civil war", "god of war", "army of the dead", "world war z","bubbles"]
+print(movie)
+print("the 12th index is",movie[11])
+movie[14] = "inception"
+print(movie)
+del movie[17]
+print(movie)
+print(movie[7:13])
+print("the last index is",movie[-1])
