3703b554e23aac98affd5b1a1eb69cf3bd5da2af
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:56:58 -0700

    dinosaurs

---
+++ dinosaurs_list.py
@@ -0,0 +1,8 @@
+dinosaur = ["Tyrannosaurus Rex", "Triceratops", "Velociraptor", "Stegosaurus", "Brachiosaurus", "Ankylosaurus", "Spinosaurus", "Diplodocus"]
+print(dinosaur)
+print("the 4th index is",dinosaur[3])
+dinosaur[5] = "Tyrannosaurus Rex"
+print(dinosaur)
+del dinosaur[6]
+print(dinosaur)
+print("the last index is", dinosaur[-1])
