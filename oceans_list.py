be285c362f590406d03ba2e436c745bddb678211
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:20:31 -0700

    oceans_list

---
+++ oceans_list.py
@@ -0,0 +1,8 @@
+oceans = ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Arctic Ocean"]
+print(oceans)
+print("the 3rd index is ",oceans[2])
+oceans[1] = "pacific oceans"
+print(oceans)
+del oceans[3]
+print(oceans)
+print("the last index is",oceans[-1])
