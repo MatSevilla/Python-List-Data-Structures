93cf1020ab74debd06ce81d7e0c267ac234b809e
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:03:26 -0700

    famous_buildings

---
+++ buildings_list.py
@@ -0,0 +1,8 @@
+buildings = ["Eiffel Tower", "Great Wall of China", "Taj Mahal", "Colosseum", "Sydney Opera House", "Burj Khalifa", "Statue of Liberty", "Pyramids of Giza"]
+print(buildings)
+print("the 5th index is",buildings[4])
+buildings[1] = "eiffel tower"
+print(buildings)
+del buildings[5]
+print(buildings)
+print("the last index is",buildings[-1])
