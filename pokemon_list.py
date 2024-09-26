09deee8a25358f3ff5f3a6ee8a5a1938ab1cb7bb
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:28:30 -0700

    Pokemon_list

---
+++ pokemon_list.py
@@ -0,0 +1,8 @@
+pokemon = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Jigglypuff", "Meowth", "Psyduck", "Machop", "Geodude", "Eevee", "Chikorita", "Cyndaquil", "Totodile", "Togepi", "Tyranitar"]
+print(pokemon)
+print("the 9th index is",pokemon[8])
+pokemon[11] = "pikachu"
+print(pokemon)
+del pokemon[9]
+print(pokemon[3:7])
+print("the last index is",pokemon[-1])
