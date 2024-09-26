db4b42ab39cc61435a6171e2122e41060ecd1baf
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:12:11 -0700

    Planet_list

---
+++ planet_list.py
@@ -0,0 +1,8 @@
+planet = ["earth", "mars", "venus", "jupiter", "saturn", "namek", "mercury", "neptune"]
+print(planet)
+print("the 3rd index is",planet[2])
+planet[6] = "pluto"
+print(planet)
+del planet[3]
+print(planet)
+print("the index is",planet[-1])
