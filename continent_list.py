567a699f05562c1ed90ff5c4dff8b378acf4896d
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:51:26 -0700

    Continent_list

---
+++ continent_list.py
@@ -0,0 +1,7 @@
+continent = ["Africa", "Asia", "North America", "South America", "Antarctica", "Europe", "Australia"]
+print(continent)
+print("the 4th index is",continent[3])
+continent[1] = "africa"
+del continent[5]
+print(continent)
+print("last index is",continent[-1])
