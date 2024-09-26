a945b988a012441d71f62811a1b0df178fbfc141
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:10:07 -0700

    My_games

---
+++ game_list.py
@@ -0,0 +1,9 @@
+game = ["valorant", "league of legends", "mobile legends", "super mario", "god of war", "bully", "gta", "the warriors", "call of duty", "counter strike", "dota", "NBA2K16","Sonic", "Need for speed", "tetris"]
+print(game)
+print("the 7th index is",game[6])
+game[3] = "minecraft"
+print(game)
+del game[8]
+print (game)
+print(game[4:10])
+print("the last index is",game[-1])
