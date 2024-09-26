d5e56611012c7214a3e0b887362c52192ba75894
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:16:11 -0700

    Habitats

---
+++ habitat_list.py
@@ -0,0 +1,8 @@
+animal_habitat = ["Forest", "Desert", "Grassland", "Wetlands", "Ocean", "Freshwater", "Tundra", "Savanna"]
+print(animal_habitat)
+print("the 5th index is",animal_habitat[4])
+animal_habitat[2] = "savannah"
+print(animal_habitat)
+del animal_habitat[5]
+print(animal_habitat)
+print("the last index is",animal_habitat[-1])
