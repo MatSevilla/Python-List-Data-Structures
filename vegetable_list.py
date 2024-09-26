0fcfe6148a2d60f0ea97f750dec6bcb6c599c608
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 19:00:41 -0700

    Vegtables

---
+++ vegetable_list.py
@@ -0,0 +1,8 @@
+vegetable = ["potato", "carrots", "cabbage", "pechay", "sayote", "celery", "onion"]
+print(vegetable)
+print("the 5th index is",vegetable[4])
+vegetable[2] = "spinach"
+print(vegetable)
+del vegetable[5]
+print(vegetable)
+print("the last index is",vegetable[-1])
