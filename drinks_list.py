1b950e5923fdcc93e608a887572fe23062fd463b
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-25 20:48:52 -0700

    Drink_list

---
+++ drinks_list.py
@@ -0,0 +1,8 @@
+drinks = ["Coffee", "Tea", "Coca-Cola", "Orange Juice", "Lemonade", "Beer", "Wine", "Sparkling Water"]
+print(drinks)
+print("the 4th index is",drinks[3])
+drinks[2] = "coffee"
+print(drinks)
+del drinks[6]
+print(drinks)
+print("the last index is",drinks[-1])
